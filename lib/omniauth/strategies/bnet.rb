4a7d1b5a7af1d6727fd84fc6f5edb284834e175e
Andrej Dragojevic <andrejdr.93@gmail.com>
2018-12-15 04:07:01 +0100

    Fix namespacing

--- lib/omniauth/strategies/bnet.rb
+++ lib/omniauth/strategies/bnet.rb
@@ -1,60 +1,62 @@
 require 'omniauth-oauth2'
 require 'base64'
 
-module OmniauthStrategies
-  class BattleNet < OmniAuth::Strategies::OAuth2
-    option :client_options, {
-      :scope => 'wow.profile'
-    }
+module OmniAuth
+  module Strategies
+    class BattleNet < OmniAuth::Strategies::OAuth2
+      option :client_options, {
+        :scope => 'wow.profile'
+      }
 
-    def client(region)
-      opts = options.client_options
-      hostname = get_host(region)
+      def client(region)
+        opts = options.client_options
+        hostname = get_host(region)
 
-      options.client_options[:authorize_url] = "https://#{hostname}/oauth/authorize" unless opts.has_key(:authorize_url)
-      options.client_options[:token_url] = "https://#{hostname}/oauth/token" unless opts.has_key(:token_url)
-      options.client_options[:site] = "https://#{hostname}/" unless opt.has_key(:site)
+        options.client_options[:authorize_url] = "https://#{hostname}/oauth/authorize" unless opts.has_key(:authorize_url)
+        options.client_options[:token_url] = "https://#{hostname}/oauth/token" unless opts.has_key(:token_url)
+        options.client_options[:site] = "https://#{hostname}/" unless opt.has_key(:site)
 
-      super
-    end
+        super
+      end
 
-    def request_phase
-      byebug
-      super
-    end
+      def request_phase
+        byebug
+        super
+      end
 
-    def authorize_params
-      super.tap do |params|
-        %w[scope client_options].each do |v|
-          if request.params[v]
-            params[v.to_sym] = request.params[v]
+      def authorize_params
+        super.tap do |params|
+          %w[scope client_options].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
           end
         end
       end
-    end
 
-    uid { raw_info['id'].to_s }
+      uid { raw_info['id'].to_s }
 
-    info do
-      raw_info
-    end
+      info do
+        raw_info
+      end
 
-    def raw_info
-      return @raw_info if @raw_info
+      def raw_info
+        return @raw_info if @raw_info
 
-      access_token.options[:mode] = :query
+        access_token.options[:mode] = :query
 
-      @raw_info = access_token.get('oauth/userinfo').parsed
-    end
+        @raw_info = access_token.get('oauth/userinfo').parsed
+      end
 
-    private
+      private
 
-    def callback_url
-      full_host + script_name + callback_path
-    end
+      def callback_url
+        full_host + script_name + callback_path
+      end
 
-    def get_host(region)
-      region == 'cn' ? 'www.battlenet.com.cn' : '#{region}.battle.net'
+      def get_host(region)
+        region == 'cn' ? 'www.battlenet.com.cn' : '#{region}.battle.net'
+      end
     end
   end
 end
